6f210875cff8a8b31851908d53a6a47ef506bd9a
Aracoder <weigel@weigelworld.org>
2015-06-03 16:58:36 -0400

    Day1 final

--- 1001Gextractor.py
+++ 1001Gextractor.py
@@ -10,10 +10,20 @@ __author__ = 'weigel'
 first_polymorphism = 0
 
 # Read a .vcf file
-
 file = open ('1001genomes_snp_short_indel_only_ACGTN_1Mb_100acc.vcf')
 
 # find first line that does not start with ##
+current_line = file.readline()
+print('First line; is it a VCF file?', current_line)
+if current_line.find('vcf', 0, len(current_line)):
+    print('This is a .vcf file!')
+else:
+    print('This is not a .vcf file!')
+    print(current_line)
+    exit()
+
+# set read pointer back to beginning of file
+file.seek(0)
 
 for line in range(100):
     first_polymorphism += 1
