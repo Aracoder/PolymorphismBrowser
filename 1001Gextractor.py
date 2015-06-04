69a843a22961b07f969dac5806ceaa3ceaa0e77a
Aracoder <weigel@weigelworld.org>
2015-06-04 12:03:00 -0400

    Noon Day 2

--- 1001Gextractor.py
+++ 1001Gextractor.py
@@ -5,6 +5,37 @@ __author__ = 'weigel'
 
 # global variables
 
+DNAbases = ['A','T','C','G']
+
+# translate SNPs into numeric codes
+# None 0000
+# A>T 2
+# A>C 3
+# A>G 4
+# T>A 10
+# T>C 30
+# T>G 40
+# C>A 100
+# C>T 200
+# C>G 400
+# G>A 1000
+# G>T 2000
+# G>C 3000
+
+# generate numeric codes
+SNPcodes = []
+for ref in range(4):
+    for alt in range(4):
+        if ref == alt:
+            SNPcodes.append(0000)
+        else:
+            SNPcodes.append(10**(ref) * (alt+1))
+
+# genrate list of SNP types consisting of two letters
+SNPs = []
+for ref in range(4):
+    for alt in range(4):
+        SNPs.append(DNAbases[ref] + DNAbases[alt])
 
 # first line with DNA data
 first_polymorphism = 0
@@ -35,18 +66,45 @@ for line in range(100):
 # set read pointer back to beginning of file
 file.seek(0)
 
-print('First line with genotype information is line ',first_polymorphism)
+print('First line with genotype information is line ',first_polymorphism, '\n')
 
 # go back to first line before polymorphism
 for line in range(first_polymorphism-1):
     file.readline()
 
-#start analyzing lines
-for line in file:
+# start analyzing lines
+
+for line in range(10):
+    # convert current_line into list elements_cl
     current_line = file.readline()
     elements_cl = current_line.split()
     print(elements_cl)
-    c_position =  
+    # positions: multiply chr with 1,000,000, add positions
+    c_pos = int(elements_cl[0]) * 1000000 + int(elements_cl[1])
+    print(c_pos)
+    # determine whether a position is a SNP or insertion or not
+    # SNPs
+    # None 0000
+    # A>T 2000
+    # A>C 3000
+    # A>G 4000
+    # T>A 0100
+    # T>C 0300
+    # T>G 0400
+    # C>A 0010
+    # C>T 0020
+    # C>G 0040
+    # G>A 0001
+    # G>T 0002
+    # G>C 0003
+    ref = elements_cl[3]
+    alt = elements_cl[4]
+    if alt in ['A','T','C','G']:
+        print("it's a SNP")
+
+
+    else:
+        print("it's not a SNP")
 
 
 
