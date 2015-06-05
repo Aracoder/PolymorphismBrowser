d172ea4543cbbcb359ab903cc4493450d8f9140f
Aracoder <weigel@weigelworld.org>
2015-06-05 13:07:35 -0400

    Counting of variants implemented

--- 1001Gextractor.py
+++ 1001Gextractor.py
@@ -8,8 +8,8 @@ __author__ = 'weigel'
 # the four bases
 DNAbases = ['A','G','C','T']
 # first line with DNA data
-first_polymorphism = 0
-snplist = []
+firstPolymorphism = 0
+snpList = []
 # number of genotypes
 samples = 0
 
@@ -18,38 +18,39 @@ samples = 0
 # substitution_types = ['AA', 'AG', 'AC', 'AT', 'GA', 'GG', 'GC', 'GT', 'CA', 'CG', 'CC', 'CT', 'TA', 'TG', 'TC', 'TT']
 # transition_transversion = ['notaSNP', 'trstn', 'trvsn', 'trvsn', 'trstn', 'notaSNP', 'trvsn', 'trvsn', 'trvsn', 'trvsn', 'notaSNP', 'trstn', 'trvsn', 'trvsn', 'trstn', 'notaSNP']
 
-substitution_types = []
-transition_transversion = []
+substitutionTypes = []
+transitionTransversion = []
 numbers = []
 for ref in range(4):
     for alt in range(4):
-        substitution_types.append(DNAbases[ref] + DNAbases[alt])
+        substitutionTypes.append(DNAbases[ref] + DNAbases[alt])
         if ref == alt:
-            transition_transversion.append('notaSNP')
+            transitionTransversion.append('notaSNP')
         elif (ref+alt) in [1,5]:
-            transition_transversion.append('trstn')
+            transitionTransversion.append('trstn')
         else:
-            transition_transversion.append('trvsn')
+            transitionTransversion.append('trvsn')
 
 
 # define class SNP
 class SNP:
-    def __init__(self, position=0, snp='', snptype='', annotation='', methylation=''):
+    def __init__(self, position=0, snp='', snpType='', snpFrequency=0, annotation='', methylation=''):
         self.p = position
         self.s = snp
-        self.t = snptype
+        self.t = snpType
+        self.f = snpFrequency
         self.a = annotation
         self.m = methylation
 
     def print(self):
-        print(self.p, self.s, self.t)
+        print(self.p, self.s, self.t, self.f, self.a, self.m)
 
 # Read a .vcf file
 file = open ('1001genomes_snp_short_indel_only_ACGTN_1Mb_100acc.vcf')
 
 # count lines in file
 with file as f:
-    for lines_in_file, l in enumerate(f):
+    for linesInFile, l in enumerate(f):
         pass
 
 file = open ('1001genomes_snp_short_indel_only_ACGTN_1Mb_100acc.vcf')
@@ -57,13 +58,13 @@ file = open ('1001genomes_snp_short_indel_only_ACGTN_1Mb_100acc.vcf')
 file.seek(0)
 
 # determine whether it's a VCF file
-current_line = file.readline()
-print('First line: ', current_line)
-if current_line.find('vcf', 0, len(current_line)):
-    print('This is a .vcf file, we are good to go.', '\n')
+currentLine = file.readline()
+print('First line: ', currentLine)
+if currentLine.find('vcf', 0, len(currentLine)):
+    print('This is a .vcf file, we are good to go.')
 else:
     print('This is not a .vcf file!')
-    print(current_line)
+    print(currentLine)
     exit()
 
 # set read pointer back to beginning of file
@@ -71,45 +72,56 @@ file.seek(0)
 
 # find header line
 for line in range(100):
-    first_polymorphism += 1
-    current_line = file.readline()
-    elements_cl = current_line.split()
-    if elements_cl[0] == '#CHROM':
-        samples = len(elements_cl) - 9
-        print("samples", samples)
+    firstPolymorphism += 1
+    currentLine = file.readline()
+    elementsCL = currentLine.split()
+    if elementsCL[0] == '#CHROM':
+        samples = len(elementsCL) - 9
+        print("There are", samples, "samples.")
         break
 
 # set read pointer back to beginning of file
 file.seek(0)
 
-print('First line with genotype information is line ',first_polymorphism, '\n')
+print('First line with genotype information is line '+str(firstPolymorphism+1)+"." '\n')
 
 # go back to first line before polymorphism
-for line in range(first_polymorphism):
+for line in range(firstPolymorphism):
     file.readline()
 
-# start analyzing lines
 
-for line in range(lines_in_file - first_polymorphism + 1):
-# for line in range(6):
+# start analyzing lines
+for line in range(linesInFile - firstPolymorphism +1):
     # convert current_line into list elements_cl
-    current_line = file.readline()
-    elements_cl = current_line.split()
+    currentLine = file.readline()
+    elementsCL = currentLine.split()
     # positions: multiply chr with 1,000,000, add positions
-    snplist.append(SNP())
-    snplist[line].p = int(elements_cl[0]) * 1000000 + int(elements_cl[1])
+    snpList.append(SNP())
+    snpList[line].p = int(elementsCL[0]) * 1000000 + int(elementsCL[1])
     # determine whether a position is a SNP or insertion or not, and if a SNP, what type of SNP
-    ref = elements_cl[3]
-    alt = elements_cl[4]
+    ref = elementsCL[3]
+    alt = elementsCL[4]
     change = ref + alt
-    if not change in substitution_types:
-        snplist[line].s = 'notaSNP'
-        snplist[line].t = 'NA'
+    if not change in substitutionTypes:
+        snpList[line].s = 'notaSNP'
+        snpList[line].t = 'NA'
     else:
-        snplist[line].s = change
-        snplist[line].t = transition_transversion[substitution_types.index(change)]
-    if int(elements_cl[1]) < 100 or int(elements_cl[1]) > 999400:
-        snplist[line].print()
+        snpList[line].s = change
+        snpList[line].t = transitionTransversion[substitutionTypes.index(change)]
+    # count number of lines with SNP
+    for accession in range(9,samples+9):
+        if elementsCL[accession].startswith('1|1'):
+            snpList[line].f += 1
+
+
+
+
+
+
+
+    if int(elementsCL[1]) < 100 or int(elementsCL[1]) > 999400:
+        snpList[line].print()
+
 
 
 
